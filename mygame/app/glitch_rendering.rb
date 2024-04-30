1ba32cbb83adccd28a46668aae8cb75f6b84c81c
Dark-Griffin <gawain@moonbow-ent.com>
2024-04-30 18:54:59 -0400

    glitch effects checkpoint

--- mygame/app/glitch_rendering.rb
+++ mygame/app/glitch_rendering.rb
@@ -3,39 +3,88 @@ def sprite_glitch(args, originalSpriteHash)
     # render glitched sprite using original sprite coordinates by rendering only chopped parts of the sprite in the square.
     divisions_for_x = 4
     divisions_for_y = 4
+    #delay of reshuffle
+    delay = 10 # in frames to display each shuffle before redoing it.
     #array of glitched sprite hashes to make chunks of sprite
     glitched_sprites = []
+
+    #if there is no glitched_sprites lookup hash, create one
+    if args.state.glitched_sprites.nil?
+        args.state.glitched_sprites = {}
+    end
+
+    #if we do not have a glitched sprite for this sprite yet, or if the timer value is 0, we need to create a new glitched sprite lookup table
+    if args.state.glitched_sprites[originalSpriteHash[:path]].nil? || args.state.glitched_sprites[originalSpriteHash[:path]][:timer] == 0
+        #create a new glitched sprite lookup table
+        args.state.glitched_sprites[originalSpriteHash[:path]] = {
+            coordinates: create_division_array_coordinates(args, divisions_for_x, divisions_for_y, originalSpriteHash),
+            timer: delay
+        }
+    end
+
+    args.state.glitched_sprites[originalSpriteHash[:path]].coordinates.each do |coordinate|
+        #iterate and draw all chunks to screen
+        glitched_sprites << {
+            x: coordinate[:x] + originalSpriteHash[:x],
+            y: coordinate[:y] + originalSpriteHash[:y],
+            w: coordinate[:w],
+            h: coordinate[:h],
+            tile_x: coordinate[:mapped_x],
+            tile_y: coordinate[:mapped_y],
+            tile_w: coordinate[:w],
+            tile_h: coordinate[:h],
+            path: originalSpriteHash[:path],
+            angle: originalSpriteHash[:angle]
+        }
+    end
+
+
+    #decrement the timer for the glitched sprite hash
+    args.state.glitched_sprites[originalSpriteHash[:path]][:timer] -= 1
+
+    #render all the glitched sprites to the screen
+    args.outputs.sprites << glitched_sprites
+end
+
+def create_division_array_coordinates(args, divisions_for_x, divisions_for_y, originalSpriteHash)
+    coordinates = []
+    #calculate width and height of a chunk
+    width = originalSpriteHash[:w] / divisions_for_x
+    height = originalSpriteHash[:h] / divisions_for_y
+
     divisions_for_x.times do |x|
         divisions_for_y.times do |y|
-            #get an x and y for a chunked part of the original sprite unique to each chunk we will draw, this is where the chunk should draw
-            #random_x_offset = x * (originalSpriteHash[:w] / divisions_for_x)
-            #random_y_offset = y * (originalSpriteHash[:h] / divisions_for_y)
-            #get a randomized "chunk coordinate" to draw inside this real location chunk
-            random_x_chunk_mapped = rand(4) * (originalSpriteHash[:w] / divisions_for_x)
-            random_y_chunk_mapped = rand(4) * (originalSpriteHash[:h] / divisions_for_y)
-            #calculate width and height of a chunk
-            width = originalSpriteHash[:w] / divisions_for_x
-            height = originalSpriteHash[:h] / divisions_for_y
-            #add a new sprite hash that is a glitched output
-            glitched_sprites << {
-                #draw at real location of chunk, but inverted position along x and y axis of sprite (so top is bottom, left is right, to emphisize glitch effect)
-                x: originalSpriteHash[:x] + (width * (divisions_for_x - x - 1)),
-                y: originalSpriteHash[:y] + (height * (divisions_for_y - y - 1)),
-                #use our calculated width and height for the chunk
+            #build a regular array of chunk locations to be scrambled
+            coordinates << { 
+                x: (originalSpriteHash[:w] / divisions_for_x) * x, 
+                y: (originalSpriteHash[:h] / divisions_for_y) * y,
                 w: width,
                 h: height,
-                #use the random chunk coordinates as a tiled chunk from the original sprite image to fill the chunk
-                tile_x: random_x_chunk_mapped,
-                tile_y: random_y_chunk_mapped,
-                tile_w: width,
-                tile_h: height,
-                path: originalSpriteHash[:path],
-                angle: originalSpriteHash[:angle]
-                #angle: 0
-                #angle: originalSpriteHash[:angle] + 180
+                #note to self: this needs to be replaced so that each chunk always has a unique x and y from another chunk in the list.
+                # most likely I need a second pass that builds a new array with the mapped_x and mapped_y values chosen off a deck list of available values, so that as it goes each value is consumed off the deck list.
+                mapped_x: x * (originalSpriteHash[:w] / divisions_for_x),
+                mapped_y: y * (originalSpriteHash[:h] / divisions_for_y)
             }
         end
     end
-    #render all the glitched sprites to the screen
-    args.outputs.sprites << glitched_sprites
+
+    #create a deck to pull mapped_x and y values from in order to avoid duplicate randoms
+    deck = []
+    #add all coordinate mapped_x and mapped_y value pairs to the deck
+    coordinates.each do |coordinate|
+        deck << { x: coordinate[:mapped_x], y: coordinate[:mapped_y] }
+    end
+    #now for each coordinate, pick one from the deck at random and overwrite the mapped_x and mapped_y values
+    coordinates.each do |coordinate|
+        #pick a random value from the deck
+        random_deck_index = rand(deck.length)
+        #overwrite the mapped_x and mapped_y values with the random value from the deck
+        coordinate[:mapped_x] = deck[random_deck_index][:x]
+        coordinate[:mapped_y] = deck[random_deck_index][:y]
+        #remove the value from the deck so it cannot be picked again
+        deck.delete_at(random_deck_index)
+    end
+
+    #now we can return the scrambled coordinates
+    return coordinates
 end
--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -8,21 +8,21 @@ def tick args
 
   args.outputs.labels  << { x: 640,
                             y: 600,
-                            text: 'Hello World!',
+                            text: 'Glitched Sprite Example! Press Space to Glitch!',
                             size_px: 30,
                             anchor_x: 0.5,
                             anchor_y: 0.5 }
 
   args.outputs.labels  << { x: 640,
                             y: 510,
-                            text: "Documentation is located under the ./docs/docs.txt directory. 150+ samples are located under the ./samples directory.",
+                            text: "require app/glitch_rendering.rb, then call sprite_glitch(args, sprite_hash) to glitch a sprite!",
                             size_px: 20,
                             anchor_x: 0.5,
                             anchor_y: 0.5 }
 
   args.outputs.labels  << { x: 640,
                             y: 480,
-                            text: "If you prefer formatted docs, you can access them locally at http://localhost:9001 or online at http://docs.dragonruby.org.",
+                            text: "This will render a glitched sprite using the original sprite coordinates by rendering only chopped parts of the sprite in the original area.",
                             size_px: 20,
                             anchor_x: 0.5,
                             anchor_y: 0.5 }
@@ -42,17 +42,29 @@ def tick args
                             anchor_y: 0.5 }
 
   if args.inputs.keyboard.space
-    sprite_glitch(args, { x: args.state.logo_rect.x, y: args.state.logo_rect.y, w: args.state.logo_rect.w, h: args.state.logo_rect.h, path: 'dragonruby.png', #angle: args.state.tick_count
-    angle: 0})
+    sprite_glitch(args, { x: args.state.logo_rect.x, y: args.state.logo_rect.y, w: args.state.logo_rect.w, h: args.state.logo_rect.h, path: 'dragonruby.png', angle: args.state.tick_count
+    #angle: 0
+    })
   else
     args.outputs.sprites << { x: args.state.logo_rect.x,
                             y: args.state.logo_rect.y,
                             w: args.state.logo_rect.w,
                             h: args.state.logo_rect.h,
                             path: 'dragonruby.png',
-                            #angle: args.state.tick_count
-                            angle: 0}
+                            angle: args.state.tick_count
+                            #angle: 0
+                          }
   end
+
+  #render a normal sprite to compare to the left of the glitched sprite x position
+  args.outputs.sprites << { x: args.state.logo_rect.x - 200,
+                            y: args.state.logo_rect.y,
+                            w: args.state.logo_rect.w,
+                            h: args.state.logo_rect.h,
+                            path: 'dragonruby.png',
+                            angle: args.state.tick_count
+                            #angle: 0
+                          }
 
   args.outputs.labels  << { x: 640,
                             y: 180,
